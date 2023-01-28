fc7821b46d4ab9f52d0eeed2dc354d2b81437ea3
masa-pocodigitalmarketing <masa@pocodigitalmarketing.com>
2023-01-28 23:18:45 +0900

    2-3

--- src/container/vwbl-container.js
+++ src/container/vwbl-container.js
@@ -1,10 +1,12 @@
 import { createContainer } from 'unstated-next';
 import { useState } from 'react';
 import Web3 from 'web3';
+import { ManageKeyType, UploadContentType, UploadMetadataType, VWBL } from 'vwbl-sdk';
 
 const useVWBL = () => {
   const [userAddress, setUserAddress] = useState('');
   const [web3, setWeb3] = useState();
+  const [vwbl, setVwbl] = useState();
 
   // Lesson-2
 	const connectWallet = async () => {
@@ -43,6 +45,9 @@ const useVWBL = () => {
           params: [{ chainId: web3.utils.toHex(properChainId) }],
         });
       }
+
+      // initVwblを実行してvwblインスタンスを作成する
+      initVwbl(web3);
 	
 	  } catch (error) {
 	
@@ -64,14 +69,30 @@ const useVWBL = () => {
     // 各変数のstateをリセット
     setUserAddress('');
     setWeb3(undefined);
+    // vwblインスタンスのstateをリセット
+    setVwbl(undefined);
   };
 
   // Lesson-3
-  const initVwbl = () => {};
+  const initVwbl = (web3) => {
+    // vwblインスタンスの作成
+    const vwblInstance = new VWBL({
+      web3,
+      contractAddress: process.env.REACT_APP_NFT_CONTRACT_ADDRESS,
+      vwblNetworkUrl: process.env.REACT_APP_VWBL_NETWORK_URL,
+      manageKeyType: ManageKeyType.VWBL_NETWORK_SERVER,
+      uploadContentType: UploadContentType.IPFS,
+      uploadMetadataType: UploadMetadataType.IPFS,
+      ipfsNftStorageKey: process.env.REACT_APP_NFT_STORAGE_KEY,
+    });
+    // vwblインスタンスをstateを保存
+    setVwbl(vwblInstance);
+  };
 
   return {
     userAddress,
     web3,
+    vwbl,
     connectWallet,
     disconnectWallet,
     initVwbl,
